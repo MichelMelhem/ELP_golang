afae4d3215e6168436269c96aafb687850e90d2a
Michel <michel.melhem@outlook.com>
2025-01-16 12:35:59 +0100

    improved the client to generate big graphs

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"elp/models"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -14,6 +15,20 @@ type DijkstraResult struct {
 	Distances map[int]int `json:"distances"`
 }
 
+func generateLargeGraph(numNodes, maxEdgesPerNode int) *models.Graph {
+	graph := models.NewGraph()
+
+	for i := 0; i < numNodes; i++ {
+		graph.AddNode(i)
+
+		for j := 1; j <= maxEdgesPerNode && i+j < numNodes; j++ {
+			graph.AddEdge(i, i+j, i+j)
+		}
+	}
+
+	return graph
+}
+
 func main() {
 	// Connexion au serveurs
 	conn, err := net.Dial("tcp", "127.0.0.1:12345")
@@ -24,18 +39,15 @@ func main() {
 	defer conn.Close()
 
 	// Création du graph
-	graphJSON := `{
-		"0": [{"To": 1, "Weight": 2}, {"To": 2, "Weight": 4}],
-		"1": [{"To": 2, "Weight": 1}, {"To": 3, "Weight": 7}],
-		"2": [{"To": 3, "Weight": 3}],
-		"3": []
-	}`
+	graphJSON, _ := json.Marshal(generateLargeGraph(25, 4))
 
-	graphJSON = strings.ReplaceAll(graphJSON, "\n", "")
-	graphJSON = strings.ReplaceAll(graphJSON, "\t", "")
+	graphAsAString := string(graphJSON)
+
+	graphAsAString = strings.ReplaceAll(graphAsAString, "\n", "")
+	graphAsAString = strings.ReplaceAll(graphAsAString, "\t", "")
 
 	// Envoie du graph sous format json au serveur
-	fmt.Fprintln(conn, graphJSON)
+	fmt.Fprintln(conn, graphAsAString)
 
 	// Lis la réponse du serveur
 	reader := bufio.NewReader(conn)
